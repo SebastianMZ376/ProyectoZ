97a44f5e35ad76a492af79f80d208226894fa46b
SebastianMZ376 <sdelamora376@accitesz.com>
2021-11-05 10:59:24 -0600

    SegundoAvance

--- ProyectoZ/controlador/app.py
+++ ProyectoZ/controlador/app.py
@@ -3,6 +3,10 @@ app = Flask(__name__, template_folder='../vista',static_folder='../static')
 
 @app.route('/')
 def main():
+    return render_template('Login.html')
+
+@app.route('/index')
+def maini():
     return render_template('comunes/index.html')
 
 @app.route('/envio')
@@ -10,12 +14,32 @@ def cin():
     return render_template('Pedidos/envio.html')
 
 @app.route('/indexx')
-def con():
-    return render_template('comunes/indexx.html')
+def pedidos():
+    return render_template('Pedidos/indexx.html')
 
 @app.route('/Carrito')
 def Carrito():
     return render_template('/Carrito/Carrito.html')
+
+@app.route('/Categorias')
+def Categoria():
+    return render_template('Categoria/Categorias.html')
+
+@app.route('/ConsulCategoria')
+def Consulta():
+    return render_template('Categoria/ConsultarCategoria.html')
+
+@app.route('/EditarCategoria')
+def Editarr():
+    return render_template('Categoria/EditarCategoria.html')
+
+@app.route('/RegistroCuenta')
+def registtro():
+    return render_template('/RegistroUsuario/RegistroCuenta.html')
+
+@app.route('/EditarC')
+def editarcuenta():
+    return render_template('/RegistroUsuario/EditarCuenta.html')
 
 @app.route('/Usuarios')
 def user():
